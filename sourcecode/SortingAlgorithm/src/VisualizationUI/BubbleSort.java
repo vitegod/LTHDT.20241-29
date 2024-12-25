54b40c27821648b98f0c4bed7c73c57b6de8dcf2
Viet Nguyen Hoang <nhv1312004@gmail.com>
2024-12-25 19:06:19 +0700

    Update BubbleSort.java

--- sourcecode/SortingAlgorithm/src/VisualizationUI/BubbleSort.java
+++ sourcecode/SortingAlgorithm/src/VisualizationUI/BubbleSort.java
@@ -21,7 +21,7 @@ public class BubbleSort extends SortingAlgorithm {
 	    int[] j = {0}; // Vòng lặp trong
 	    boolean[] comparing = {false}; // Trạng thái có đang so sánh hay không
 
-	    KeyFrame keyFrame = new KeyFrame(Duration.millis(1500), event -> {
+	    KeyFrame keyFrame = new KeyFrame(Duration.millis(300), event -> {
 	        if (i[0] < myArray.getNbElements() - 1) {
 	            if (!comparing[0]) {
 	                // Đánh dấu hai phần tử được so sánh
@@ -31,9 +31,7 @@ public class BubbleSort extends SortingAlgorithm {
 	            } else {
 	                // Thực hiện so sánh và hoán đổi nếu cần
 	                if (elements[j[0]].getValue() > elements[j[0] + 1].getValue()) {
-	                    double temp = elements[j[0]].getValue();
-	                    elements[j[0]].setValueText(elements[j[0] + 1].getValue());
-	                    elements[j[0] + 1].setValueText(temp);
+	                	swap(j[0], j[0] + 1);
 	                    
 	                    String[] info = {"Cause array[" + j[0] + "] > array[" + (j[0] + 1) + "], swap them."};
 	                    this.instructionList.getItems().addAll(info);
@@ -66,4 +64,17 @@ public class BubbleSort extends SortingAlgorithm {
 	    timeline.getKeyFrames().add(keyFrame);
 	    timeline.play();
 	}
+	
+	private void swap(int index1, int index2) {
+        double tempValue = elements[index1].getValue();
+        elements[index1].setValueText(elements[index2].getValue());
+        elements[index2].setValueText(tempValue);
+
+        double tempHeight = elements[index1].getHeight();
+        elements[index1].setHeight(elements[index2].getHeight());
+        elements[index2].setHeight(tempHeight);
+        
+        elements[index1].getRectangle().setHeight(elements[index1].getHeight());
+        elements[index2].getRectangle().setHeight(elements[index2].getHeight());
+    }
 }
