7a18156ceb16173b1c984be85f97eb4722dd58bd
Viet Nguyen Hoang <nhv1312004@gmail.com>
2024-12-25 19:06:22 +0700

    Update InsertionSort.java

--- sourcecode/SortingAlgorithm/src/VisualizationUI/InsertionSort.java
+++ sourcecode/SortingAlgorithm/src/VisualizationUI/InsertionSort.java
@@ -20,12 +20,15 @@ public class InsertionSort extends SortingAlgorithm{
 	    int[] i = {1}; // Vị trí hiện tại trong vòng lặp ngoài
 	    int[] j = {0}; // Vị trí hiện tại trong vòng lặp while
 	    double[] key = {0}; // Giá trị của phần tử cần chèn
+	    double[] keyHeight = {0};
 
 	    // Thêm các bước vào Timeline
-	    timeline.getKeyFrames().add(new KeyFrame(Duration.millis(1500), event -> {
+	    timeline.getKeyFrames().add(new KeyFrame(Duration.millis(300), event -> {
 	        if (j[0] < 0 || elements[j[0]].getValue() <= key[0]) {
 	            // Chèn phần tử vào vị trí thích hợp
-	            elements[j[0] + 1].setValueText(key[0]);
+	        	elements[j[0] + 1].setValueText(key[0]);
+                elements[j[0] + 1].setHeight(keyHeight[0]);
+                
 	            String[] info1 = {"Key element: " + " array[" + i[0] + "] = " + key[0] + "."};
                 this.instructionList.getItems().addAll(info1);
 	            String[] info = {"Insert key element into position: index = " + (j[0] + 1) + "."};
@@ -46,6 +49,7 @@ public class InsertionSort extends SortingAlgorithm{
 	                }
 	            } else {
 	                key[0] = elements[i[0]].getValue();
+	                keyHeight[0] = elements[i[0]].getHeight();
 	                j[0] = i[0] - 1;
 	                elements[i[0]].getRectangle().setFill(Color.YELLOW); // Đánh dấu phần tử đang xử lý
 	                
@@ -53,6 +57,7 @@ public class InsertionSort extends SortingAlgorithm{
 	        } else {
 	            // Di chuyển phần tử lớn hơn sang phải
 	            elements[j[0] + 1].setValueText(elements[j[0]].getValue());
+	            elements[j[0] + 1].setHeight(elements[j[0]].getHeight());
 	            elements[j[0]].getRectangle().setFill(Color.YELLOW);
 	            j[0]--;
 	        }
@@ -60,6 +65,7 @@ public class InsertionSort extends SortingAlgorithm{
 
 	    // Bắt đầu thực thi Timeline
 	    key[0] = elements[i[0]].getValue();
+	    keyHeight[0] = elements[i[0]].getHeight();
 	    j[0] = i[0] - 1;
 	    elements[i[0]].getRectangle().setFill(Color.YELLOW); // Đánh dấu phần tử đầu tiên
 	    timeline.play();
